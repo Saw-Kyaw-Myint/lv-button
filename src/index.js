fd87de056d5fda268b9f54d955796bfba2c14d16
Saw Kyaw Myint <scm.sawkyawmyint@gmail.com>
2023-11-23 21:25:27 +0630

    empty data

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import HelloWorld from "./components/HelloWorld.vue";
-
-export { HelloWorld };
-
-export default {
-  install: (app, options) => {
-    app.component("HelloWorld", HelloWorld);
-  },
-};
